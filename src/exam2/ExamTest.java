a5062342780c4d1eb5e849bf4ef7929bb874138b
Jeongyusu <Jeongyusu@naver.com>
2023-06-05 12:24:18 +0900

    자바 소스파일

---
+++ src/exam2/ExamTest.java
@@ -0,0 +1,8 @@
+package exam2;
+
+public class ExamTest {
+    public static void main(String[] args) {
+        System.out.println((int) (0.02 * 5000));
+    }
+
+}
---
+++ src/practice/Elevator.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Elevator {
+    public static void main(String[] args) {
+
+        int a = 13;
+        int b = 7;
+        int c = 10;
+        
+        System.out.println();
+    }
+    
+}
